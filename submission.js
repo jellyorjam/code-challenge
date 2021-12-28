9f46b2636f79464b9a89b0a933e05e12d3e3ed0e
jellyorjam <92885877+jellyorjam@users.noreply.github.com>
2021-12-28 11:04:31 -0500

    Create submission.js

---
+++ submission.js
@@ -0,0 +1,107 @@
+const findSum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
+
+const findFrequency = function(array) {
+  let counter = 0;
+  let amount = 0;
+  let mostFrequentItem;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] === array[j]) {
+        counter++; 
+      }
+      if (counter > amount) {
+        amount = counter;
+        mostFrequentItem = array[j];
+      } 
+    }
+    counter = 0;
+  }
+const leastFrequentItem = array.sort((a,b) =>
+          array.filter(v => v===b).length
+        - array.filter(v => v===a).length
+    ).pop();
+
+const frequencyObj = {};
+  frequencyObj['most'] = mostFrequentItem;
+  frequencyObj['least'] = leastFrequentItem;
+
+  return frequencyObj;
+};
+
+const isPalindrome = function(str) {
+  let reverseWord = str.toLowerCase().split('').reverse().join('')
+  if (reverseWord === str.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const largestPair = function(array) {
+  let x = 0;
+  let y = 0;
+  let product = 0;
+  let highestProduct = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    x = array[i];
+    y = array [i + 1];
+    product = x * y;
+    if (product > highestProduct) {
+      highestProduct = product;
+    }
+  }
+  return highestProduct;
+};
+
+const removeParenth = function(str) {
+  let newString = str.split('');
+  let newArray = [];
+  let counter = 0;
+  for (let i = 0; i < newString.length; i++) {
+    if (newString[i] === '(') {
+      counter++;
+    }
+    if (counter === 0) {
+      newArray.push(newString[i])
+    }
+    if (newString[i] === ')') {
+      counter--;
+    }
+  }
+  return newArray.join('');
+};
+
+const scoreScrabble = function(str) {
+  let score = 0;
+  for (let i = 0; i < str.length; i++) {
+   if (str[i] === 'a' || str[i] === 'e' || str[i] === 'o' || str[i] === 'i' || str[i] === 'u' || str[i] === 'l' || str[i] === 'n' || str[i] === 'r' || str[i] === 's' ||str[i] === 't') {
+     score += 1;
+   }
+   if (str[i] === 'd' || str[i] === 'g') {
+     score += 2;
+   }
+   if (str[i] === 'b' || str[i] === 'c' || str[i] === 'm' || str[i] === 'p') {
+     score += 3;
+   }
+   if (str[i] === 'f' || str[i] === 'h' || str[i] === 'v' || str[i] === 'w' || str[i] === 'y') {
+     score += 4;
+   }
+   if (str[i] === 'k') {
+     score += 5;
+   }
+   if (str[i] === 'j' || str[i] === 'x') {
+     score += 8;
+   }
+   if (str[i] === 'q' || str[i] === 'z') {
+     score += 10;
+   }
+  }
+  return score;
+};
